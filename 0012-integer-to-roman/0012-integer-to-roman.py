15fb0987f070e5670d3755f53f602723f97f38fc
abhishekkk-kumar <95019546+abhishekkk-kumar@users.noreply.github.com>
2022-10-20 11:23:50 +0530

    Time: 94 ms (53.14%), Space: 13.8 MB (80.12%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,22 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        res = ""
+        table = [(1000, "M"),
+                (900, "CM"),
+                (500, "D"),
+                (400, "CD"),
+              (100, "C"),
+              (90, "XC"),
+              (50, "L"),
+              (40, "XL"),
+              (10, "X"),
+              (9, "IX"),
+              (5, "V"),
+              (4, "IV"),
+              (1, "I")]
+        for cap, roman in table:
+            d, num= divmod(num,cap)
+            res += roman * d
+            #num = m
+
+        return res
